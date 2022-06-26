0af7c327f8785698f434efcd0150ea1a6b253b23
Kirill Taran <kirilltaran@hotmail.com>
2022-06-26 17:18:55 +0300

    Sentry added

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/gin-gonic/gin v1.8.1 // direct
 
 require (
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/getsentry/sentry-go v0.13.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
--- startup/startup.go
+++ startup/startup.go
@@ -2,12 +2,28 @@ package startup
 
 import (
 	"main/routers"
+	"main/utils"
 
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
 )
 
 func Configure() *gin.Engine {
+	err := sentry.Init(sentry.ClientOptions{
+		AttachStacktrace: true,
+		Dsn:              utils.GetEnvironmentVariable("SENTRY_DSN"),
+		TracesSampleRate: 1.0,
+	})
+	if err != nil {
+		log.Error().Err(err).Msgf("Sentry initialization failed: %v", err.Error())
+	}
+
 	app := gin.Default()
+	app.Use(sentrygin.New(sentrygin.Options{
+		Repanic: true,
+	}))
 	routers.SetupRouters(app)
 
 	return app
