d01e713658be5583320e7b3064982a7b0cd8a5a0
Kirill Taran <kirilltaran@hotmail.com>
2022-09-11 15:24:33 +0400

    Reverts startup changes

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"main/infrastructure"
 	"main/infrastructure/consul"
 	"main/services/common"
 	"main/startup"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 //"os"
@@ -20,38 +28,38 @@ func main() {
 	consul := consul.BuildConsulClient(logger, "service-api")
 
 	hostSettings := consul.Get("HostSettings").(map[string]interface{})
-	//hostAddress := hostSettings["Address"]
-	//hostPort := hostSettings["Port"]
+	hostAddress := hostSettings["Address"]
+	hostPort := hostSettings["Port"]
 	ginMode := hostSettings["Mode"].(string)
 
 	app := startup.Configure(logger, consul, ginMode)
 	app.Run()
 
-	// server := &http.Server{
-	// 	Addr:    fmt.Sprintf("%s:%s", hostAddress, hostPort),
-	// 	Handler: app,
-	// }
+	server := &http.Server{
+		Addr:    fmt.Sprintf("%s:%s", hostAddress, hostPort),
+		Handler: app,
+	}
 
-	//go func() {
-	// logger.LogInfo("Starting...")
-	// err := server.ListenAndServe()
-	// if err != nil && errors.Is(err, http.ErrServerClosed) {
-	// 	logger.LogError(err, "Listen error: %s\n", err.Error())
-	// }
-	//}()
+	go func() {
+		logger.LogInfo("Starting...")
+		err := server.ListenAndServe()
+		if err != nil && errors.Is(err, http.ErrServerClosed) {
+			logger.LogError(err, "Listen error: %s\n", err.Error())
+		}
+	}()
 
-	//quit := make(chan os.Signal, 1)
-	//signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	//<-quit
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
 
 	logger.LogInfo("Sutting down...")
 
-	//ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
-	//defer cancelFunc()
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
 
-	//if err := server.Shutdown(ctx); err != nil {
-	//	logger.LogError(err, "Server forced to shutdown: %s", err)
-	//}
+	if err := server.Shutdown(ctx); err != nil {
+		logger.LogError(err, "Server forced to shutdown: %s", err)
+	}
 
 	logger.LogInfo("Exiting")
 }
