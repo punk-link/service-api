98055a8601403dc31f3354e2efe2e020be1c89b9
Kirill Taran <kirilltaran@hotmail.com>
2023-01-17 00:13:42 +0400

    Contracts updated

--- services/artists/converters/grpc_release_converter.go
+++ services/artists/converters/grpc_release_converter.go
@@ -21,12 +21,14 @@ func ToReleaseMessage(err error, release artistData.Release, artists []artistDat
 
 	return &presentationContracts.Release{
 		Id:             int32(release.Id),
+		Description:    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 		ImageDetails:   imageDetails,
 		Label:          release.Label,
 		Name:           release.Name,
 		PlatformUrls:   platformUrlMessages,
 		ReleaseArtists: slimArtists,
 		ReleaseDate:    timestamppb.New(release.ReleaseDate),
+		Tags:           []string{"indie", "post-punk"},
 		Tracks:         tracks,
 		Type:           release.Type,
 	}, nil
